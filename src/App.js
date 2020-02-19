921d4d168b5b2879de6a41ce3dc171c953e8a5e4
nsns0101 <nsns0101@naver.com>
2020-02-19 23:09:34 +0900

    ReactHooks-functionComponent--Count

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from "react";
 
-export default class App extends React.Component {
-  state = {
-    item: 0
-  };
+/* React */
+// class App extends React.Component {
+//   state = {
+//     item: 0
+//   };
 
-  incrementItem = () => {
-    this.setState({
-      item: this.state.item + 1
-    });
-  };
-  decrementItem = () => {
-    this.setState({
-      item: this.state.item - 1
-    });
-  };
+//   incrementItem = () => {
+//     this.setState({
+//       item: this.state.item + 1
+//     });
+//   };
+//   decrementItem = () => {
+//     this.setState({
+//       item: this.state.item - 1
+//     });
+//   };
 
-  render() {
-    const { item } = this.state;
-    console.log(item);
-    return (
-      <div className="App">
-        <h2>Hello {item}</h2>
-        <button onClick={this.incrementItem}>incrementItem</button>
-        <button onClick={this.decrementItem}>decrementItem</button>
-      </div>
-    );
-  }
-}
+//   render() {
+//     const { item } = this.state;
+//     console.log(item);
+//     return (
+//       <div className="App">
+//         <h2>Hello {item}</h2>
+//         <button onClick={this.incrementItem}>incrementItem</button>
+//         <button onClick={this.decrementItem}>decrementItem</button>
+//       </div>
+//     );
+//   }
+// }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+//React Hooks
+const App2 = () => {
+  const [item, setItem] = useState(0);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h2>Hello {item}</h2>
+      <button onClick={incrementItem}>incrementItem</button>
+      <button onClick={decrementItem}>decrementItem</button>
+    </div>
+  );
+};
+
+export default App2;
