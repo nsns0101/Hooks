4e6a0af07fb9b98bfc25c133acf8583039b4d29d
nsns0101 <nsns0101@naver.com>
2020-02-20 01:04:57 +0900

    useTabs-buttonClick

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
 import React, { useState } from "react";
 
-const useInput = (initialValue, validator) => {
-  const [value, setVaule] = useState(initialValue);
+const content = [
+  {
+    id: 1,
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    id: 2,
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  }
+];
 
-  //글 입력이 가능한 이벤트
-  const onChange = event => {
-    const {
-      target: { value }
-    } = event;
-
-    let willUpdate = true; //글 업데이트가 가능한지(글 입력이 가능한지)
-
-    //validator이 함수면
-    if (typeof validator === "function") {
-      willUpdate = validator(value); //validator가 maxLen이라는 함수니까
-      console.log(willUpdate); //글이 10글자가 넘어가면 false를 리턴할 것임
-    }
-    //willUpdate가 true면 (즉, 10글자가 넘어가면 글이 안써짐)
-    if (willUpdate) {
-      setVaule(value);
-    }
+const useTabs = (initialTab, allTabs) => {
+  //allTabs가 false 또는 allTabs가 배열이 아닐 때
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
   };
-
-  return { value, onChange };
 };
 
 const App = () => {
-  const maxLen = value => value.length <= 10;
-  //const maxLen = value => value.includes("@");      //value안에 @가 있으면 글이 안써지도록
-  const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className="App">
-      <h2>Hello</h2>
-      {/* <input placeholder="Name" value={name.value} onChange={name.onChange} /> */}
-      <input placeholder="Name" {...name} />
+      {content.map((section, index) => (
+        //changeItem은 useTabs안의 changeItem을 가르키고
+        //그 changeItem은 setCurrentIndex를 가르키니
+        //결과적으로 setCurrentIndex(index)와 같음
+        <button key={index} onClick={() => changeItem(index)}>
+          {section.tab}
+        </button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
