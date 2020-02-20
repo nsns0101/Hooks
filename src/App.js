7aa17c8cdfb3f8ed0d5b5121096cf8e7c8923900
nsns0101 <nsns0101@naver.com>
2020-02-20 23:49:07 +0900

    useConfirm

--- src/App.js
+++ src/App.js
@@ -1,55 +1,31 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 
-const useClick = onClick => {
-  console.log("2"); //const title = useClick(sayHello); 코드로 실행
-  //유효성검사
-  // if (typeof onClick !== "function") {
-  //   return;
-  // }
-  const element = useRef(); // 밑의 <h1 ref={title}>Hi</h1>가 얘를 가르킴
-  console.log(element);
-  if (element.current) {
-    console.log("zzz");
+const useConfirm = (message = "", delete_yes, delete_no) => {
+  //callback가 function이 아니면 아무것도 리턴을 하지 않음
+  if (typeof delete_yes !== "function" && delete_no !== "function") {
+    return;
   }
-  useEffect(() => {
-    console.log("4"); //App의 return에서 html이 그려짐에 따라 실행
 
-    //componentDidMount, componentWillUpdate할 때
-    //밑의 return문을 보면 이벤트를 다시 삭제하는데
-    //그 이유는 component가 mount되지 않았을 때 eventListener이 배치되지 않게 하기 위해
-    if (element.current) {
-      console.log("5"); //이것도 App의 return에서 html이 그려짐에 따라 실행하는 듯
-      //element.current = h1
-      //h1태그 클릭시 eee함수 실행
-      element.current.addEventListener("click", onClick);
+  const confirmAction = () => {
+    //window.confirm = 경고창과 비슷한 예, 아니오를 누를 수 있는 창
+    if (window.confirm(message)) {
+      //예를 누르면
+      delete_yes(); //callback함수가 실행
+      //=> callback && 는 결과적으로 deleteWorld함수
+    } else {
+      delete_no();
     }
-
-    return () => {
-      //componentWillUnMount 할 때
-      if (element.current) {
-        element.current.removeEventListener("click", onClick);
-      }
-    };
-  }, []);
-
-  return element;
+  };
+  return confirmAction;
 };
 
 const App = () => {
-  console.log("1"); //export default로 실행
-  const sayHello = () => console.log("say Hello");
-  const title = useClick(sayHello);
-  console.log("3"); //23번째 줄 종료 후 실행
-
-  const a = () => {
-    console.log("a");
-  };
+  const deleteWorld = () => console.log("Deleting the world..."); //삭제 : 예
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure", deleteWorld, abort);
   return (
     <div className="App">
-      {/* ref속성은 리액트에서 DOM에 직접적인 접근을 할 때 쓰임  */}
-      {/* 1. title은 h1에 접근을 할 수 있음 */}
-      <h1 ref={title}>Hi</h1>
-      <h2 ref={a}>hh</h2>
+      <button onClick={confirmDelete}>Delete the world</button>
     </div>
   );
 };
