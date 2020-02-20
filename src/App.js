1eb7f3d3f1534871f2d454491d3c0e00a7566dd1
nsns0101 <nsns0101@naver.com>
2020-02-20 15:28:42 +0900

    useRef cursor

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import React, { useState, useEffect } from "react";
-
-const useTitle = initialTitle => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    //document.querySelector = 특정 id나 class 상관없이 element를 찾음
-    const htmlTitle = document.querySelector("title"); // <title>Loading...</title>
-    htmlTitle.innerText = title; // Loading...
-  };
-
-  //title 값이 바뀌면 updateTitle함수 실행
-  useEffect(updateTitle, [title]);
-
-  return setTitle;
-};
+import React, { useState, useEffect, useRef } from "react";
 
 const App = () => {
-  const titleUpdator = useTitle("Loading...");
-  setTimeout(function() {
-    return titleUpdator("Home");
-  }, 2000);
+  const potato = useRef();
+  setTimeout(() => potato.current.focus(), 2000);
+
   return (
     <div className="App">
       <div>Hi</div>
+      <input ref={potato} placeholder="la"></input>
     </div>
   );
 };
