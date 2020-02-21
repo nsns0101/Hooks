f8686112c59e125ca6422a5afa2606644044664a
nsns0101 <nsns0101@naver.com>
2020-02-21 15:36:45 +0900

    useEffect - useFadeIn

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-//useBeforeLeave
-//마우스 커서가 페이지 공간을 벗어나면 실행하는 함수
-import React, { useEffect } from "react";
+//useFadeIn
+//서서히 나타나는 것
+import React, { useEffect, useRef } from "react";
 
-const useBeforeLeave = onBefore => {
-  // if (typeof onBefore !== "function") {
+//duration => 나타내는 속도, delay함수가 일어나는 속도
+const useFadeIn = (duration = 1, delay = 0) => {
+  // if (typeof duration !== "number" || typeof delay !== "number") {
   //   return;
   // }
-  const handle = event => {
-    // console.log(event); // handle는 mouseleave 이벤트
-    console.log(event.clientY); //페이지를 벗어날 때의 마우스 y좌표(위로 벗어나면 숫자가 커짐)
-    //마우스의 Y좌표가 위로 벗어났을 때만
-    if (event.clientY <= 0) {
-      onBefore();
-    }
-  };
-
+  const element = useRef(); //element는 실행시킨 함수의 ref태그를 가르킴?
   useEffect(() => {
-    document.addEventListener("mouseleave", handle);
-
-    return () => document.removeEventListener("mouseleave", handle);
+    if (element.current) {
+      const { current } = element;
+      //transition = 애니메이션 효과
+      //style=`transition: `opacity ${duration}s ease-in-out ${delay}s`
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`; //드러나는 속도
+      //style=`opacity:${duration}`
+      current.style.opacity = 1; //불투명도
+    }
   }, []);
+
+  return { ref: element, style: { opacity: 0 } };
 };
 
 const App = () => {
-  const begForLife = () => console.log("Please dont leave");
-  useBeforeLeave(begForLife);
+  const fadeInH1 = useFadeIn(1, 2);
+  const fadeInP = useFadeIn(5, 3);
   return (
     <div className="App">
-      <h1>hello</h1>
+      {/* opacity는 불투명도  0이면 안보임 */}
+      {/* <h1 ref={fadeInH1.ref} style={fadeInH1.style}> hello</h1>*/}
+      <h1 {...fadeInH1}>Hello</h1>
+      <p {...fadeInP}>lorem ipsum lalala</p>
     </div>
   );
 };
