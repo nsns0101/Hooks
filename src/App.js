04cbe985bdc4175129cda1a08e2a548b43ec2e0c
nsns0101 <nsns0101@naver.com>
2020-02-21 16:20:58 +0900

    useEffect - useScroll

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
-//useNetwork
-//네비게이터가 온라인 또는 오프라인 되는 것을 방지
-//네트워크 상태가 Online인지 Offline인지를 판단
+//useScroll
+//유저 스크롤을 감지 x는 필요없음
 import React, { useEffect, useState } from "react";
 
-//네트워크를 판단하는 함수
-const useNetwork = onChange => {
-  //navigator.onLine는 브라우저의 온라인 상태를 반환함
-  console.log(navigator.onLine); //온라인이면 true를 반환
-  const [status, setStatus] = useState(navigator.onLine);
+const useScroll = () => {
+  const [state, setState] = useState({
+    x: 0,
+    y: 0
+  });
 
-  const handleChange = () => {
-    if (typeof onChange === "function") {
-      //onChange는 App의 handleNetworkChange함수
-      onChange(navigator.onLine); //true면 console.log("We juse went online"); 실행
-    }
-    setStatus(navigator.onLine); //네트워크값이 바뀌는 것을 감지
+  const onScroll = () => {
+    console.log(`x : ${window.scrollX} \n y : ${window.scrollY}`);
+    setState({
+      x: window.scrollX, //화면의 스크롤 x좌표
+      y: window.scrollY //화면의 스크롤 y좌표
+    });
   };
 
   useEffect(() => {
-    window.addEventListener("online", handleChange); //window에 online이벤트 생성
-    window.addEventListener("offline", handleChange); //window에 offline이벤트 생성
+    window.addEventListener("scroll", onScroll); //스크롤 이벤트 생성
 
-    return () => {
-      window.removeEventListener("online", handleChange);
-      window.removeEventListener("offline", handleChange);
-    };
+    return () => window.removeEventListener("scroll", onScroll); //스크롤 이벤트 삭제
   }, []);
 
-  return status;
+  return state;
 };
 
 const App = () => {
-  const handleNetworkChange = online => {
-    console.log(online ? "We juse went online" : "We are offline");
-  };
-  const online = useNetwork(handleNetworkChange);
+  const { y } = useScroll();
 
   return (
-    <div className="App">
-      <h1>{online ? "Online" : "Offline"}</h1>
+    <div className="App" style={{ height: "1000vh" }}>
+      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>
+        Hello
+      </h1>
     </div>
   );
 };
